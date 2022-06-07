33d4d0b6bc05a38e58760c77a56eb38e9b4fa9ee
Mayron Alves <mayron@devsquad.com>
2022-06-06 23:00:20 -0600

    COC-6: installed alpinejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,8 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+Alpine.start();
