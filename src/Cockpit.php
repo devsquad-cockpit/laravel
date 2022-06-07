540ffb86218cf6656f52f7e741aeff96b87c2161
Mayron Alves <mayron@devsquad.com>
2022-06-06 19:52:42 -0600

    COC-3: dumping the result to the Laravel Ray software

--- src/Cockpit.php
+++ src/Cockpit.php
@@ -2,6 +2,12 @@
 
 namespace Cockpit;
 
+use Throwable;
+
 class Cockpit
 {
+    public static function handle(Throwable $throwable, $fileType = 'php', array $customData = [])
+    {
+        ray($throwable);
+    }
 }
--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -4,26 +4,14 @@ namespace Cockpit\Exceptions;
 
 use Cockpit\Cockpit;
 use Monolog\Handler\AbstractProcessingHandler;
-use Monolog\Logger;
+use Throwable;
 
 class Handler extends AbstractProcessingHandler
 {
-    protected Cockpit $cockpit;
-
-    public function __construct(Cockpit $cockpit, int $level = Logger::ERROR, bool $bubble = true)
-    {
-        $this->cockpit = $cockpit;
-
-        parent::__construct($level, $bubble);
-    }
-
-    /**
-     * @param array $record
-     */
     protected function write(array $record): void
     {
         if (isset($record['context']['exception']) && $record['context']['exception'] instanceof Throwable) {
-            // TODO: Implement cockpit exception handling
+            Cockpit::handle($record['context']['exception']);
         }
     }
 }
