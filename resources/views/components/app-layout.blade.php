337fe20fe3095afbae60c98554b9aa828edb1afd
Mayron Alves <mayron@devsquad.com>
2022-06-07 19:25:05 -0600

    COC-4: fixed page width

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -13,7 +13,7 @@
     <div class="min-h-full">
         <x-cockpit::nav/>
 
-        <div class="max-w-7xl mx-auto py-10">
+        <div class="max-w-7xl mx-auto py-10 px-4 sm:px-6 lg:px-8">
             {{ $slot }}
         </div>
     </div>
