aa1e26f65a600b027fddbdfe71542b7809a77eb9
Matheus Lopes Santos <matheussantos@devsquad.com>
2022-06-10 14:58:19 -0300

    COC-7: Added error nav and link component
    Implements: COC-7

---
+++ resources/views/components/error/nav/link.blade.php
@@ -0,0 +1,11 @@
+@props([
+    'href'   => '#',
+    'active' => null,
+    'mobile' => null
+])
+
+<a href="" {{ $attributes->class([
+    'hover:bg-dark text-white hover:text-primary group flex items-center px-2 py-2 rounded-md font-thin'
+]) }}>
+    {{ $slot }}
+</a>
