8f85a308269414a5c6ab546fc0fdc031940b4e70
Matheus Lopes Santos <matheussantos@devsquad.com>
2022-06-13 09:32:04 -0300

    COC-7: Added `suggestion` component
    Implements: COC-7

---
+++ resources/views/components/error/suggestion.blade.php
@@ -0,0 +1,24 @@
+<div class="mt-8">
+    <x-cockpit::alert green outline>
+        <div class="w-0 flex-1 flex items-center">
+            <x-cockpit::icons.light-bulb/>
+            <p class="ml-3 font-medium truncate">
+                <span class="inline">3 possible solutions found</span>
+            </p>
+        </div>
+        <div class="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+            <a href="#"
+               class="text-sm flex items-center justify-center px-3 text-sm ">
+                <x-cockpit::icons.arrow-down class="mr-3 h-3 w-3"/>
+                View
+            </a>
+        </div>
+        <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
+            <a href="#"
+               class="text-sm flex items-center justify-center text-sm ">
+                <x-cockpit::icons.x class="mr-3 h-4 w-4"/>
+                Dismiss
+            </a>
+        </div>
+    </x-cockpit::alert>
+</div>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -30,30 +30,7 @@
         <x-cockpit::card.error-status title="Affected Users" value="12"/>
     </div>
 
-    <div class="mt-8">
-        <x-cockpit::alert green outline>
-            <div class="w-0 flex-1 flex items-center">
-                <x-cockpit::icons.light-bulb/>
-                <p class="ml-3 font-medium truncate">
-                    <span class="inline">3 possible solutions found</span>
-                </p>
-            </div>
-            <div class="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-                <a href="#"
-                   class="text-sm flex items-center justify-center px-3 text-sm ">
-                    <x-cockpit::icons.arrow-down class="mr-3 h-3 w-3"/>
-                    View
-                </a>
-            </div>
-            <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
-                <a href="#"
-                   class="text-sm flex items-center justify-center text-sm ">
-                    <x-cockpit::icons.x class="mr-3 h-4 w-4"/>
-                    Dismiss
-                </a>
-            </div>
-        </x-cockpit::alert>
-    </div>
+    <x-cockpit::error.suggestion />
 
     <div class="grid grid-cols-4 gap-4 mt-8">
         <x-cockpit::error.nav/>
