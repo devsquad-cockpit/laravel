86f724482b1e7e2ee15af0cea08e0246490404ed
Matheus Lopes Santos <matheussantos@devsquad.com>
2022-06-14 15:56:20 -0300

    COC-7: Added option that saves a new error in database
    Implements: COC-7

--- src/Cockpit.php
+++ src/Cockpit.php
@@ -8,6 +8,36 @@ class Cockpit
 {
     public static function handle(Throwable $throwable, $fileType = 'php', array $customData = [])
     {
-        ray($throwable);
+        $error = \Cockpit\Models\Error::create([
+            'exception'      => get_class($throwable),
+            'occurrences'    => 1,
+            'affected_users' => 1,
+        ]);
+
+        $error->occurrences()->create([
+            'url'     => app('request')->fullUrl(),
+            'type'    => 'web',
+            'message' => $throwable->getMessage(),
+            'code'    => $throwable->getCode(),
+            'file'    => $throwable->getFile(),
+            'trace'   => static::getTrace($throwable->getTrace()),
+        ]);
+    }
+
+    protected static function getTrace(array $stackTrace): array
+    {
+        $trace = [];
+
+        foreach ($stackTrace as $item) {
+            $trace[] = [
+                'file'     => $item['file'],
+                'line'     => $item['line'],
+                'function' => $item['function'],
+                'class'    => $item['class'] ?? null,
+                'preview'  => FileContext::getContext($item['file'], $item['line'])
+            ];
+        }
+
+        return $trace;
     }
 }
