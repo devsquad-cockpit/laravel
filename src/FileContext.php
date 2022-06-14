ab9738612c5cc60733c4cf8a02dd8118033ca36e
Matheus Lopes Santos <matheussantos@devsquad.com>
2022-06-14 11:39:09 -0300

    COC-7: Added `FileContext` class and its tests
    Implements: COC-7

---
+++ src/FileContext.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Cockpit;
+
+class FileContext
+{
+    public static function getContext(string $filePath, int $line): array
+    {
+        if (!file_exists($filePath)) {
+            return [$line => ''];
+        }
+
+        return collect(explode("\n", file_get_contents($filePath)))
+            ->slice($line - 10, 20)
+            ->mapWithKeys(function ($value, $key) {
+                return [$key + 1 => $value];
+            })->all();
+    }
+}
