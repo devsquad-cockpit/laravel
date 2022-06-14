af704b2b267ac8569ed5789474f197b4a9ca271d
Matheus Lopes Santos <matheussantos@devsquad.com>
2022-06-14 15:53:23 -0300

    COC-7: Added `SubstituteBindings` middleware on cockpit routes
    Implements: COC-7

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Cockpit\Http\Controllers\CockpitController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/cockpit', [CockpitController::class, 'index'])->name('cockpit.index');
-Route::get('/cockpit/{cockpit}', [CockpitController::class, 'show'])->name('cockpit.show');
+Route::middleware([
+    \Illuminate\Routing\Middleware\SubstituteBindings::class,
+])->group(function () {
+    Route::get('/cockpit', [CockpitController::class, 'index'])->name('cockpit.index');
+    Route::get('/cockpit/{occurrence}', [CockpitController::class, 'show'])->name('cockpit.show');
+});
