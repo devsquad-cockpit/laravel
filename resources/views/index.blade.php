162db1112dde4525462fb0a0266229853c668c32
Mayron Alves <mayron@devsquad.com>
2022-06-13 19:30:16 -0600

    COC-4: fixing mobile layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,8 @@
 <x-cockpit::app-layout>
-    <div class="grid grid-cols-3 gap-4 items-center">
+    <div class="grid sm:grid-cols-3 gap-4 items-center">
         <x-cockpit::card.dashboard title="Errors per day" total="71897" percentage="12"/>
         <x-cockpit::card.dashboard title="Errors in the last hour" total="71897" percentage="12"/>
         <x-cockpit::card.dashboard title="Unresolved errors" total="71897" percentage="12"/>
     </div>
 </x-cockpit::app-layout>
+
