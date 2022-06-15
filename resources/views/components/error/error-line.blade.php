deaa0a5f35e629341a0ed8cd213f9a0fed3334b1
Matheus Lopes Santos <matheussantos@devsquad.com>
2022-06-15 12:06:15 -0300

    COC-7: Added button to open editor for user
    Implements: COC-7

--- resources/views/components/error/error-line.blade.php
+++ resources/views/components/error/error-line.blade.php
@@ -1,12 +1,16 @@
 <div class="w-full overflow-scroll">
     <template x-for="(code, line) in exceptionSelected.preview">
-        <div class="stack-trace flex flex-grow group cursor-pointer">
-            <div class="bg-[#393D3F] flex items-center text-left px-2 sticky left-0"
+        <div class="stack-trace flex flex-grow group">
+            <div class="flex items-center text-left px-2 sticky left-0"
+                 x-bind:class="line == exceptionSelected.line ? 'bg-red-500' : 'bg-[#393D3F]'"
                  x-text="line">
             </div>
 
-            <div class="text-sm whitespace-normal py-1 px-3">
-                <pre x-html="code"></pre>
+            <div class="text-sm whitespace-normal py-1 px-3 flex items-center"
+                 x-bind:class="line == exceptionSelected.line ? 'bg-red-600' : ''"
+            >
+                <x-cockpit::error.editor-link />
+                <pre class="mr-3" x-html="code"></pre>
             </div>
         </div>
     </template>
