d3bfee481036708dd2ab3e1de78d7af6835ec0ec
Matheus Lopes Santos <matheussantos@devsquad.com>
2022-06-16 10:15:21 -0300

    COC-7: Improve animation when changing tab view

--- resources/views/components/error/stacktrace/index.blade.php
+++ resources/views/components/error/stacktrace/index.blade.php
@@ -1,12 +1,4 @@
-<x-cockpit::error.section
-    x-transition:enter="transition ease-out duration-300"
-    x-transition:enter-start="opacity-0 scale-90"
-    x-transition:enter-end="opacity-100 scale-100"
-    x-transition:leave="transition ease-in duration-300"
-    x-transition:leave-start="opacity-100 scale-100"
-    x-transition:leave-end="opacity-0 scale-90"
-    {{ $attributes }}
->
+<x-cockpit::error.section {{ $attributes }}>
     <div class="grid grid-cols-3">
         <div class="p-4 w-full">
             <!-- Frames -->
