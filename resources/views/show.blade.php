5eafbdf64cab74162acf4bee5f31924d146e8f2f
Matheus Lopes Santos <matheussantos@devsquad.com>
2022-06-16 11:55:53 -0300

    COC-7: Added button that will mark an error as resolved

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,4 +1,4 @@
-<x-cockpit::app-layout>
+<x-cockpit::app-layout xmlns:x-cockpit="http://www.w3.org/1999/html">
     @php
         /** @var \Cockpit\Models\Error $cockpitError */
     @endphp
@@ -12,34 +12,46 @@
         {{ $cockpitError->exception }}: {{ $cockpitError->message }}
     </x-cockpit::error.error-title>
 
-    <span class="text-gray-900 dark:text-white text-sm">
-        <div class="flex items-center">
-            <x-cockpit-icons icon="link" class="mr-3"/>
-            {{ $cockpitError->url }}
+    <div class="flex justify-between">
+        <span class="text-gray-900 dark:text-white text-sm">
+            <div class="flex items-center">
+                <x-cockpit-icons icon="link" class="mr-3"/>
+                {{ $cockpitError->url }}
+            </div>
+        </span>
+    </div>
+
+    <div class="flex justify-between items-center mt-8">
+        <div class="flex gap-7 text-sm">
+            <x-cockpit::card.error-status
+                title="Latest Occurrence"
+                value="{{ $cockpitError->getOccurrenceTime() }}"
+                description="{{ $cockpitError->getOccurrenceDescription() }}"
+            />
+
+            <x-cockpit::card.error-status
+                title="First Occurrence"
+                :value="$cockpitError->created_at->toFormattedDateString()"
+            />
+
+            <x-cockpit::card.error-status
+                title="# of occurrences"
+                :value="$cockpitError->occurrences"
+            />
+
+            <x-cockpit::card.error-status
+                title="Affected Users"
+                :value="$cockpitError->affected_users"
+            />
         </div>
-    </span>
-
-    <div class="grid grid-cols-4 gap-3 items-center mt-6">
-        <x-cockpit::card.error-status
-            title="Latest Occurrence"
-            value="{{ $cockpitError->getOccurrenceTime() }}"
-            description="{{ $cockpitError->getOccurrenceDescription() }}"
-        />
-
-        <x-cockpit::card.error-status
-            title="First Occurrence"
-            :value="$cockpitError->created_at->toFormattedDateString()"
-        />
-
-        <x-cockpit::card.error-status
-            title="# of occurrences"
-            :value="$cockpitError->occurrences"
-        />
-
-        <x-cockpit::card.error-status
-            title="Affected Users"
-            :value="$cockpitError->affected_users"
-        />
+        <form action="#">
+            {{ csrf_field() }}
+            <x-cockpit::button
+                green
+            >
+                Mark as Resolved
+            </x-cockpit::button>
+        </form>
     </div>
 
     <div class="grid grid-cols-5 gap-4 mt-8" x-data="tab('stackTrace')">
