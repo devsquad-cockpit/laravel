7d79c326266e78e8638d024475022c3ef4aaa4df
Mayron Alves <mayron@devsquad.com>
2022-06-21 10:03:14 -0600

    COC-9: fixing user tab

--- resources/views/components/error/user.blade.php
+++ resources/views/components/error/user.blade.php
@@ -23,11 +23,10 @@
             </x-cockpit::error.section.content>
         @endisset
 
-        <x-cockpit::error.section.content type="User Data" copyable code-type="json">
-            @if (is_array($error->user))
+        @if (is_array($error->user))
+            <x-cockpit::error.section.content type="User Data" copyable code-type="json">
                 @json(\Illuminate\Support\Arr::except($error->user, 'guard'))
-            @endif
-        </x-cockpit::error.section.content>
-
+            </x-cockpit::error.section.content>
+        @endif
     </x-cockpit::error.section.wrapper>
 </x-cockpit::error.section>
