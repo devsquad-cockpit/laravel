97f4bab73a1c6f2d910ec374a6c565d6eed633f0
Matheus Lopes Santos <matheussantos@devsquad.com>
2022-06-24 11:15:44 -0300

    COC-13: Fixed code standard

--- resources/views/components/error/command.blade.php
+++ resources/views/components/error/command.blade.php
@@ -6,6 +6,7 @@
         <x-cockpit::error.section.content type="Command">
             {{ $error->command['command'] ?? null }}
         </x-cockpit::error.section.content>
+
         <x-cockpit::error.section.content type="Arguments" copyable code-type="json">
             @json($error->command['arguments'] ?? [])
         </x-cockpit::error.section.content>
