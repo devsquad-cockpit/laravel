aa8dabc4ef109b4ea84c19dfa9a7acc3f3e53f45
Matheus Lopes Santos <matheussantos@devsquad.com>
2022-07-07 10:32:35 -0300

    COC-20: Moved main functions from `Cockpit` to `CockpitErrorHandler`

--- src/Exceptions/CockpitErrorHandler.php
+++ src/Exceptions/CockpitErrorHandler.php
@@ -3,6 +3,8 @@
 namespace Cockpit\Exceptions;
 
 use Cockpit\Cockpit;
+use Cockpit\Context\{AppContext, CommandContext, JobContext, LivewireContext, StackTraceContext, UserContext};
+use Cockpit\Models\Error;
 use Monolog\Handler\AbstractProcessingHandler;
 use Throwable;
 
@@ -10,8 +12,79 @@ class CockpitErrorHandler extends AbstractProcessingHandler
 {
     protected function write(array $record): void
     {
-        if (isset($record['context']['exception']) && $record['context']['exception'] instanceof Throwable) {
-            Cockpit::handle($record['context']['exception']);
+        if (!$this->shouldReport($record)) {
+            return;
         }
+
+        Cockpit::handle($record['context']['exception']);
+    }
+
+    protected function shouldReport(array $report): bool
+    {
+        return $this->hasException($report);
+    }
+
+    protected function hasException(array $report): bool
+    {
+        return isset($report['context']['exception']) && $report['context']['exception'] instanceof Throwable;
+    }
+
+    protected function log(Throwable $throwable)
+    {
+        $traceContext    = app(StackTraceContext::class, ['throwable' => $throwable]);
+        $userContext     = app(UserContext::class, ['hiddenFields' => Cockpit::$userHiddenFields]);
+        $appContext      = app(AppContext::class, ['throwable' => $throwable]);
+        $commandContext  = app(CommandContext::class);
+        $livewireContext = app(LivewireContext::class);
+        $jobContext      = app(JobContext::class);
+
+        /** @var Error $error */
+        $error = Error::query()->firstOrNew([
+            'exception'   => get_class($throwable),
+            'message'     => $throwable->getMessage(),
+            'resolved_at' => null,
+        ]);
+
+        $error->fill([
+            'type'               => $this->getExceptionType(),
+            'url'                => $this->resolveUrl(),
+            'code'               => $throwable->getCode(),
+            'file'               => $throwable->getFile(),
+            'trace'              => $traceContext->getContext(),
+            'user'               => $userContext->getContext(),
+            'app'                => $appContext->getContext(),
+            'command'            => $commandContext->getContext(),
+            'livewire'           => $livewireContext->getContext(),
+            'job'                => $jobContext->getContext(),
+            'occurrences'        => $error->occurrences + 1,
+            'affected_users'     => $this->calculateAffectedUsers($error),
+            'last_occurrence_at' => now(),
+        ])->save();
+    }
+
+    protected function calculateAffectedUsers(Error $error): int
+    {
+        return app()->runningInConsole() ? $error->affected_users : $error->affected_users + 1;
+    }
+
+    protected function resolveUrl(): ?string
+    {
+        return !app()->runningInConsole()
+            ? app('request')->fullUrl()
+            : null;
+    }
+
+    protected function getExceptionType(): string
+    {
+        if (!app()->runningInConsole()) {
+            return Error::TYPE_WEB;
+        }
+
+        return $this->isExceptionFromJob() ? Error::TYPE_JOB : Error::TYPE_CLI;
+    }
+
+    protected function isExceptionFromJob(): bool
+    {
+        return is_array(app(JobContext::class)->getContext());
     }
 }
