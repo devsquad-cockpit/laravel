2563aa2e356a84588abf8ea067870e2126ed0950
Matheus Lopes Santos <matheussantos@devsquad.com>
2022-07-13 10:30:45 -0300

    COC-11: Added dump context

---
+++ src/Context/DumpContext.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Cockpit\Context;
+
+use Cockpit\Context\Dump\Dump;
+use Cockpit\Context\Dump\DumpHandler;
+use Cockpit\Context\Dump\HtmlDumper;
+use Cockpit\Context\Dump\MultiDumpHandler;
+use Cockpit\Interfaces\ContextInterface;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Arr;
+use ReflectionMethod;
+use ReflectionProperty;
+use Symfony\Component\VarDumper\Cloner\Data;
+use Symfony\Component\VarDumper\VarDumper;
+
+class DumpContext implements ContextInterface
+{
+    protected array $dumps = [];
+
+    protected Application $app;
+
+    protected static bool $registeredHandler = false;
+
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    public function start(): DumpContext
+    {
+        $multiDumpHandler = new MultiDumpHandler();
+
+        $this->app->singleton(MultiDumpHandler::class, fn () => $multiDumpHandler);
+
+        if (!self::$registeredHandler) {
+            static::$registeredHandler = true;
+
+            $this->ensureOriginalHandlerExists();
+
+            $originalHandler = VarDumper::setHandler(fn ($dumpedVariable) => $multiDumpHandler->dump($dumpedVariable));
+
+            $multiDumpHandler->addHandler($originalHandler);
+            $multiDumpHandler->addHandler(fn ($var) => (new DumpHandler($this))->dump($var));
+        }
+
+        return $this;
+    }
+
+    public function record(Data $data)
+    {
+        $backtrace   = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 11);
+        $sourceFrame = $this->findSourceFrame($backtrace);
+
+        $file       = (string)Arr::get($sourceFrame, 'file');
+        $lineNumber = (int)Arr::get($sourceFrame, 'line');
+
+        $htmlDump = (new HtmlDumper())->dump($data);
+
+        $this->dumps[] = new Dump($htmlDump, $file, $lineNumber);
+    }
+
+    public function reset()
+    {
+        $this->dumps = [];
+    }
+
+    public function getContext(): ?array
+    {
+        $dumps = [];
+
+        foreach ($this->dumps as $dump) {
+            $dumps[] = $dump->toArray();
+        }
+
+        return $dumps;
+    }
+
+    protected function ensureOriginalHandlerExists(): void
+    {
+        $reflectionProperty = new ReflectionProperty(VarDumper::class, 'handler');
+        $reflectionProperty->setAccessible(true);
+
+        $handler = $reflectionProperty->getValue();
+
+        if (!$handler) {
+            $reflectionMethod = new ReflectionMethod(VarDumper::class, 'register');
+            $reflectionMethod->setAccessible(true);
+            $reflectionMethod->invoke(null);
+        }
+    }
+
+    protected function findSourceFrame(array $stackTrace): ?array
+    {
+        $seenVarDumper = false;
+
+        foreach ($stackTrace as $frame) {
+            if (Arr::get($frame, 'class') === VarDumper::class && Arr::get($frame, 'function') === 'dump') {
+                $seenVarDumper = true;
+
+                continue;
+            }
+
+            if (!$seenVarDumper) {
+                continue;
+            }
+
+            return $frame;
+        }
+
+        return null;
+    }
+}
