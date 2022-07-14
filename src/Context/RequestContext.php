683bd652b0f8669d89fe2c151996285053ca9069
Mayron Alves <mayron@devsquad.com>
2022-07-13 23:51:46 -0600

    COC-11: retrieved and mapped all content to the request column in the database.

--- src/Context/RequestContext.php
+++ src/Context/RequestContext.php
@@ -5,6 +5,10 @@ namespace Cockpit\Context;
 use Cockpit\Interfaces\ContextInterface;
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Collection;
+use RuntimeException;
+use Symfony\Component\Mime\Exception\InvalidArgumentException;
 
 class RequestContext implements ContextInterface
 {
@@ -20,20 +24,108 @@ class RequestContext implements ContextInterface
     public function getContext(): ?array
     {
         return [
-            'url'          => $this->request->fullUrl(),
-            'method'       => $this->request->method(),
-            'curl'         => $this->getCurl(),
+            'request' => [
+                'url'    => $this->request->url(),
+                'method' => $this->request->method(),
+                'curl'   => $this->getCurl(),
+            ],
             'headers'      => $this->request->headers->all(),
             'query_string' => $this->request->query->all(),
-            'body'         => $this->request->all(),
-            'files'        => $this->request->allFiles(),
+            'body'         => $this->getBody(),
+            'files'        => $this->getFiles(),
             'session'      => $this->request->getSession(),
-            'cookies'      => $this->request->cookies->all(),
+            'cookies'      => $this->getCookies(),
         ];
     }
 
     protected function getCurl(): string
     {
-        return '';
+        return <<<SHELL
+    curl "{$this->request->url()}" \
+    -X {$this->request->method()} \
+{$this->getCurlHeaders()}{$this->getCurlBody()}
+SHELL;
+    }
+
+    protected function getCurlHeaders(): string
+    {
+        $headers    = "";
+        $allHeaders = $this->request->headers->all();
+        foreach ($allHeaders as $header => $value) {
+            $value = implode(',', $value);
+            $headers .= "\t-H '{$header}: {$value}' \ \r\n";
+        }
+
+        return $headers;
+    }
+
+    protected function getCurlBody(): string
+    {
+        $body    = "";
+        $allBody = $this->getBody();
+        $lastKey = array_key_last($allBody);
+        foreach ($allBody as $label => $value) {
+            $body .= "\t-F '{$label}={$value}'";
+            if ($label != $lastKey) {
+                $body .= " \ \r\n";
+            }
+        }
+
+        return $body;
+    }
+
+    protected function getBody(): array
+    {
+        return $this->request->except(array_merge(
+            ['_token'],
+            $this->request->query->all(),
+            array_keys($this->getFiles())
+        ));
+    }
+
+    protected function getFiles(): array
+    {
+        if (is_null($this->request->files)) {
+            return [];
+        }
+
+        return $this->mapFiles($this->request->files->all());
+    }
+
+    protected function mapFiles(array $files): array
+    {
+        return array_map(function ($file) {
+            if (is_array($file)) {
+                return $this->mapFiles($file);
+            }
+
+            if (!$file instanceof UploadedFile) {
+                return [];
+            }
+
+            try {
+                $fileSize = $file->getSize();
+            } catch (RuntimeException $e) {
+                $fileSize = 0;
+            }
+
+            try {
+                $mimeType = $file->getMimeType();
+            } catch (InvalidArgumentException $e) {
+                $mimeType = 'undefined';
+            }
+
+            return [
+                'pathname' => $file->getPathname(),
+                'size'     => $fileSize,
+                'mimeType' => $mimeType,
+            ];
+        }, $files);
+    }
+
+    protected function getCookies(): Collection
+    {
+        return collect($this->request->cookies->all())
+            ->except(['XSRF-TOKEN', 'laravel_session']);
     }
 }
