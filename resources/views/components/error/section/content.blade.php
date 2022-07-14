3d8b22c7daf44f468acf7b1571c16bbec1d368cd
Mayron Alves <mayron@devsquad.com>
2022-07-13 23:52:21 -0600

    COC-11: changed the copyable component to accept nowrap content.

--- resources/views/components/error/section/content.blade.php
+++ resources/views/components/error/section/content.blade.php
@@ -1,11 +1,11 @@
-@props(['type', 'copyable' => null, 'codeType' => null])
+@props(['type', 'copyable' => null, 'codeType' => null, 'wrap' => true])
 
 <div class="text-right font-bold tracking-wider">
     {{ $type }}
 </div>
 <div class="col-span-3">
     @if ($copyable && $codeType)
-        <x-cockpit::error.section.copyable :code-type="$codeType">
+        <x-cockpit::error.section.copyable :code-type="$codeType" :wrap="$wrap">
             {{ $slot }}
         </x-cockpit::error.section.copyable>
     @else
