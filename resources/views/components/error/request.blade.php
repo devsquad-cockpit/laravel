58323a1610cedc865b08b684329659dfcfe4e4b3
Mayron Alves <mayron@devsquad.com>
2022-07-13 23:53:52 -0600

    COC-12: loaded the request content into the blade file.

--- resources/views/components/error/request.blade.php
+++ resources/views/components/error/request.blade.php
@@ -3,36 +3,26 @@
 @php /** @var \Cockpit\Models\Occurrence $occurrence */ @endphp
 
 <x-cockpit::error.section {{ $attributes }} class="p-4">
-    <x-cockpit::error.section.wrapper title="Request">
-        <x-cockpit::error.section.content type="URL"></x-cockpit::error.section.content>
-        <x-cockpit::error.section.content type="Method"></x-cockpit::error.section.content>
-        <x-cockpit::error.section.content type="Curl"></x-cockpit::error.section.content>
-    </x-cockpit::error.section.wrapper>
-
-    <x-cockpit::error.section.wrapper title="Headers">
-        <x-cockpit::error.section.content type="Cookie"></x-cockpit::error.section.content>
-        <x-cockpit::error.section.content type="Accept-Language"></x-cockpit::error.section.content>
-        <x-cockpit::error.section.content type="Accept-Encoding"></x-cockpit::error.section.content>
-        <x-cockpit::error.section.content type="Something"></x-cockpit::error.section.content>
-    </x-cockpit::error.section.wrapper>
-
-    <x-cockpit::error.section.wrapper title="Query String">
-        <x-cockpit::error.section.content type="Something"></x-cockpit::error.section.content>
-    </x-cockpit::error.section.wrapper>
-
-    <x-cockpit::error.section.wrapper title="Body">
-        <x-cockpit::error.section.content type="Something"></x-cockpit::error.section.content>
-    </x-cockpit::error.section.wrapper>
-
-    <x-cockpit::error.section.wrapper title="Files">
-        <x-cockpit::error.section.content type="Something"></x-cockpit::error.section.content>
-    </x-cockpit::error.section.wrapper>
-
-    <x-cockpit::error.section.wrapper title="Session">
-        <x-cockpit::error.section.content type="Something"></x-cockpit::error.section.content>
-    </x-cockpit::error.section.wrapper>
-
-    <x-cockpit::error.section.wrapper title="Cookies">
-        <x-cockpit::error.section.content type="Something"></x-cockpit::error.section.content>
-    </x-cockpit::error.section.wrapper>
+    @foreach ($occurrence->request as $title => $sectionData)
+        <x-cockpit::error.section.wrapper :title="Str::spaceTitle($title)">
+            @foreach ($sectionData as $type => $content)
+                @if (is_array($content))
+                    @if ($title == 'files')
+                        <x-cockpit::error.section.content :type="$type" copyable="true" code-type="json">
+                            @json($content)
+                        </x-cockpit::error.section.content>
+                    @else
+                        <x-cockpit::error.section.content :type="$type" copyable="true" code-type="text" :wrap="false">
+                            {{ implode(',', $content) }}
+                        </x-cockpit::error.section.content>
+                    @endif
+                @else
+                    <x-cockpit::error.section.content :type="$title == 'request' ? Str::title($type) : $type"
+                                                      copyable="true" code-type="shell">
+                        {{ $content }}
+                    </x-cockpit::error.section.content>
+                @endif
+            @endforeach
+        </x-cockpit::error.section.wrapper>
+    @endforeach
 </x-cockpit::error.section>
