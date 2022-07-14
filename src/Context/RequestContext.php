9f96e3172f9552320fde539e2909935ba3263062
Mayron Alves <mayron@devsquad.com>
2022-07-13 21:44:32 -0600

    COC-11: added a method to get the necessary data from the browser request

--- src/Context/RequestContext.php
+++ src/Context/RequestContext.php
@@ -4,18 +4,36 @@ namespace Cockpit\Context;
 
 use Cockpit\Interfaces\ContextInterface;
 use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 
 class RequestContext implements ContextInterface
 {
     protected Application $app;
+    protected Request     $request;
 
     public function __construct(Application $app)
     {
-        $this->app = $app;
+        $this->app     = $app;
+        $this->request = $this->app->make(Request::class);
     }
 
     public function getContext(): ?array
     {
-        return [];
+        return [
+            'url'          => $this->request->fullUrl(),
+            'method'       => $this->request->method(),
+            'curl'         => $this->getCurl(),
+            'headers'      => $this->request->headers->all(),
+            'query_string' => $this->request->query->all(),
+            'body'         => $this->request->all(),
+            'files'        => $this->request->allFiles(),
+            'session'      => $this->request->getSession(),
+            'cookies'      => $this->request->cookies->all(),
+        ];
+    }
+
+    protected function getCurl(): string
+    {
+        return '';
     }
 }
