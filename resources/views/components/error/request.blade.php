d2504810fe39c00d01290b4f0234e9fad65faf34
Mayron Alves <mayron@devsquad.com>
2022-07-13 21:34:50 -0600

    COC-11: added the initial content to handle the request data

--- resources/views/components/error/request.blade.php
+++ resources/views/components/error/request.blade.php
@@ -1,3 +1,38 @@
+@props(['occurrence'])
+
+@php /** @var \Cockpit\Models\Occurrence $occurrence */ @endphp
+
 <x-cockpit::error.section {{ $attributes }} class="p-4">
-    Here we are in request
+    <x-cockpit::error.section.wrapper title="Request">
+        <x-cockpit::error.section.content type="URL"></x-cockpit::error.section.content>
+        <x-cockpit::error.section.content type="Method"></x-cockpit::error.section.content>
+        <x-cockpit::error.section.content type="Curl"></x-cockpit::error.section.content>
+    </x-cockpit::error.section.wrapper>
+
+    <x-cockpit::error.section.wrapper title="Headers">
+        <x-cockpit::error.section.content type="Cookie"></x-cockpit::error.section.content>
+        <x-cockpit::error.section.content type="Accept-Language"></x-cockpit::error.section.content>
+        <x-cockpit::error.section.content type="Accept-Encoding"></x-cockpit::error.section.content>
+        <x-cockpit::error.section.content type="Something"></x-cockpit::error.section.content>
+    </x-cockpit::error.section.wrapper>
+
+    <x-cockpit::error.section.wrapper title="Query String">
+        <x-cockpit::error.section.content type="Something"></x-cockpit::error.section.content>
+    </x-cockpit::error.section.wrapper>
+
+    <x-cockpit::error.section.wrapper title="Body">
+        <x-cockpit::error.section.content type="Something"></x-cockpit::error.section.content>
+    </x-cockpit::error.section.wrapper>
+
+    <x-cockpit::error.section.wrapper title="Files">
+        <x-cockpit::error.section.content type="Something"></x-cockpit::error.section.content>
+    </x-cockpit::error.section.wrapper>
+
+    <x-cockpit::error.section.wrapper title="Session">
+        <x-cockpit::error.section.content type="Something"></x-cockpit::error.section.content>
+    </x-cockpit::error.section.wrapper>
+
+    <x-cockpit::error.section.wrapper title="Cookies">
+        <x-cockpit::error.section.content type="Something"></x-cockpit::error.section.content>
+    </x-cockpit::error.section.wrapper>
 </x-cockpit::error.section>
