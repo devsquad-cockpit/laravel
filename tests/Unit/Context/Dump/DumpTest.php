42b3c25248b1950c9289bc6f837d38b2f95458ed
Gustavo Araujo <gusttavo212@gmail.com>
2022-08-08 14:43:50 -0300

    Test Dump class

---
+++ tests/Unit/Context/Dump/DumpTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Cockpit\Tests\Unit\Context\Dump;
+
+use Cockpit\Context\Dump\Dump;
+
+it('should mount valid dump data', function () {
+    $sourceFrame = [
+        "file"     => "Cockpit\Tests\Unit\Context\Dump\VarDumper.php",
+        "line"     => 123,
+        "function" => "dump",
+        "class"    => VarDumper::class,
+        "type"     => "->"
+    ];
+    $dump = new Dump(getHtmlString(), $sourceFrame['file'], $sourceFrame['line'], 123);
+
+    expect($dump)->toBeInstanceOf(Dump::class);
+    expect($dump->toArray())
+        ->toBeArray()
+        ->toMatchArray([
+            'html_dump'   => getHtmlString(),
+            'file'        => $sourceFrame['file'],
+            'line_number' => $sourceFrame['line'],
+            'microtime'   => '123'
+        ]);
+});
+
+function getHtmlString():string
+{
+    return
+        <<<'EOTXT'
+        <foo></foo><bar>"<span class=sf-dump-str title="9 characters">Text dump</span>"
+        </bar>
+        EOTXT;
+}
