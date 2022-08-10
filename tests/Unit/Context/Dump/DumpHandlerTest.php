7043baaa4a3fda9e503dfa19074a218ecf8a1236
Gustavo Araujo <gusttavo212@gmail.com>
2022-08-10 09:52:28 -0300

    Add Dump Handler Test

---
+++ tests/Unit/Context/Dump/DumpHandlerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Cockpit\Tests\Unit\Context\Dump;
+
+use Cockpit\Context\Dump\DumpHandler;
+use Cockpit\Context\DumpContext;
+
+it('should be execute dump handler record value at dump context', function () {
+    $value       = "Text dump";
+    $dumpContext = $this->app->make(DumpContext::class);
+
+    expect($dumpContext->getContext())->toBeEmpty()->toBeArray()->toHaveCount(0);
+
+    $dumpHandler = new DumpHandler($dumpContext);
+    $dumpHandler->dump($value);
+
+    $response = $dumpContext->getContext()[0];
+
+    expect($response)->toBeArray()->toHaveKeys(
+        ['html_dump', 'file', 'line_number', 'microtime']
+    );
+    expect($response['html_dump'])->toBeString()->toContain($value);
+});
