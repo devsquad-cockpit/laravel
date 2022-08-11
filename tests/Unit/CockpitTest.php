2be31910fb176456d08d5cd5dd1d8d0a040d230e
Gustavo Araujo <gusttavo212@gmail.com>
2022-08-11 14:10:44 -0300

    Create test to Cockpit

---
+++ tests/Unit/CockpitTest.php
@@ -0,0 +1,28 @@
+<?php
+
+// namespace Tests\Unit\Middleware;
+
+use Cockpit\Cockpit;
+use Illuminate\Http\Request;
+
+it('should be check auth with success', function ($value) {
+    $cockpit = app(Cockpit::class);
+    $cockpit->auth(fn () => $value);
+
+    expect($cockpit->check(new Request))
+        ->toBeBool()
+        ->toBe($value);
+})->with([
+    true,
+    false
+]);
+
+it('should be set and get user hidden fields', function () {
+    $cockpit = app(Cockpit::class);
+    $cockpit->setUserHiddenFields(['password', 'email']);
+
+    expect($cockpit->getUserHiddenFields())
+        ->toBeArray()
+        ->toHaveCount(2)
+        ->toMatchArray(['password', 'email']);
+});
