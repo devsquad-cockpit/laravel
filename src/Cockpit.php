7d4ab02c4ac258f11978e4d615ee9bb0a8c7659f
Gustavo Araujo <gusttavo212@gmail.com>
2022-08-12 09:44:13 -0300

    Remove get function to static prop

--- src/Cockpit.php
+++ src/Cockpit.php
@@ -16,11 +16,6 @@ class Cockpit
         static::$userHiddenFields = $userHiddenFields;
     }
 
-    public static function getUserHiddenFields(): array
-    {
-        return static::$userHiddenFields;
-    }
-
     public static function check(Request $request)
     {
         return (static::$authUsing ?: function () {
