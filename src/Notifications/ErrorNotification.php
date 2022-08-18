d919272e89d2a4dd099db96ee4f7cdee475f2786
Gustavo Araujo <gusttavo212@gmail.com>
2022-08-18 10:13:09 -0300

    Add WebHook Channel notification

--- src/Notifications/ErrorNotification.php
+++ src/Notifications/ErrorNotification.php
@@ -6,6 +6,8 @@ use Cockpit\Models\Error;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use NotificationChannels\Webhook\WebhookChannel;
+use NotificationChannels\Webhook\WebhookMessage;
 
 class ErrorNotification extends Notification
 {
@@ -20,7 +22,7 @@ class ErrorNotification extends Notification
 
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['mail', WebhookChannel::class];
     }
 
     public function toMail()
@@ -34,5 +36,19 @@ class ErrorNotification extends Notification
             ->line('A new error has been registered in Cockpit. You can check details about the error below or, if you prefer, click on the "Error Details" button to be redirected to the Cockpit.')
             ->line($description)
             ->action('View Error Details', $this->error->url);
+    }
+
+    public function toWebhook($notifiable)
+    {
+        $description = sprintf('%s: %s', $this->error->exception, $this->error->message);
+
+        return WebhookMessage::create()
+            ->data([
+                'payload' => [
+                    'webhook' => $description
+                ]
+            ])
+            ->userAgent("Custom-User-Agent")
+            ->header('X-Custom', 'Custom-Header');
     }
 }
