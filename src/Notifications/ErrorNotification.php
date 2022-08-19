0a70b86459030ad7b3539da99c12ac802c1df8de
Gustavo Araujo <gusttavo212@gmail.com>
2022-08-19 10:38:34 -0300

    Add telegram notification chanel

--- src/Notifications/ErrorNotification.php
+++ src/Notifications/ErrorNotification.php
@@ -7,6 +7,7 @@ use Cockpit\Models\Error;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use NotificationChannels\Telegram\TelegramMessage;
 use NotificationChannels\Webhook\WebhookChannel;
 use NotificationChannels\Webhook\WebhookMessage;
 
@@ -24,9 +25,10 @@ class ErrorNotification extends Notification
     public function via($notifiable)
     {
         return [
-          'mail', 
-          WebhookChannel::class,
-          CustomSlackChannel::class,
+            'mail',
+            WebhookChannel::class,
+            CustomSlackChannel::class,
+            'telegram'
         ];
     }
 
@@ -45,20 +47,33 @@ class ErrorNotification extends Notification
 
     public function toWebhook($notifiable)
     {
-        $description = sprintf('%s: %s', $this->error->exception, $this->error->message);
-
         return WebhookMessage::create()
             ->data([
                 'payload' => [
-                    'webhook' => $description
+                    'webhook' => [
+                        'id'          => $this->error->id,
+                        'url'         => $this->error->url,
+                        'description' => sprintf('%s: %s', $this->error->exception, $this->error->message),
+                    ]
                 ]
             ])
-            ->userAgent("Cockpit-User-Agent")
-            ->header('X-Cockpit', 'Cockpit-Header');
+        ->userAgent("Cockpit-User-Agent")
+        ->header('X-Cockpit', 'Cockpit-Header');
     }
 
     public function toCustomSlack()
     {
         return $this->error;
+    }
+
+    public function toTelegram($notifiable)
+    {
+        $description = sprintf('%s: %s', $this->error->exception, $this->error->message);
+
+        return TelegramMessage::create()
+            ->token(config('cockpit.notifications.telegram.token'))
+            ->to(config('cockpit.notifications.telegram.to'))
+            ->content('A new error has been registered in Cockpit. You can check details click on the "Error Details" button to be redirected to the Cockpit. ' . $description)
+            ->button('Error Details', $this->error->url);
     }
 }
