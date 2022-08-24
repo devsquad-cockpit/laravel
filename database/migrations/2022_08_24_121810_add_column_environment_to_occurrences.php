7280e984dbe3afd0f671fc1d8400f5d5708b27b1
Gustavo Araujo <gusttavo212@gmail.com>
2022-08-24 09:37:22 -0300

    Add new field environment to occurrences table

---
+++ database/migrations/2022_08_24_121810_add_column_environment_to_occurrences.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnEnvironmentToOccurrences extends Migration
+{
+    public function up():void
+    {
+        Schema::table('occurrences', function (Blueprint $table) {
+            $table->json('environment')->default('[]');
+        });
+    }
+    public function down():void
+    {
+        Schema::table('occurrences', function (Blueprint $table) {
+            $table->dropColumn('environment');
+        });
+    }
+};
