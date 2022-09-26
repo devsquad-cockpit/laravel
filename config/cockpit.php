8628e971f72b7a54e157f1c53d5129002e0f7ebb
devsquad-andre-lunelli <andre.lunelli@devsquad.com>
2022-09-26 11:50:48 -0300

    COC-144: align vars

--- config/cockpit.php
+++ config/cockpit.php
@@ -2,6 +2,6 @@
 
 return [
     'enabled' => env('COCKPIT_ENABLED', true),
-    'domain' => env('COCKPIT_DOMAIN'),
-    'token' => env('COCKPIT_TOKEN'),
+    'domain'  => env('COCKPIT_DOMAIN'),
+    'token'   => env('COCKPIT_TOKEN'),
 ];
