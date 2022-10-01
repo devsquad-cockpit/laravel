c36a53321ecb5453c82b18ed1371595e3ba17047
AJ Meireles <alvaro.meireles@devsquad.com>
2022-09-30 21:45:14 -0300

    [Feature] COC-162: Laravel Client Configuration Description

--- config/cockpit.php
+++ config/cockpit.php
@@ -1,7 +1,33 @@
 <?php
 
 return [
+    /*
+     |--------------------------------------------------------------------------
+     | Status
+     |--------------------------------------------------------------------------
+     |
+     | This setting controls the Cockpit status.
+     |
+     */
     'enabled' => env('COCKPIT_ENABLED', true),
-    'domain'  => env('COCKPIT_DOMAIN'),
-    'token'   => env('COCKPIT_TOKEN'),
+
+    /*
+     |--------------------------------------------------------------------------
+     | Domain
+     |--------------------------------------------------------------------------
+     |
+     | This setting refers to the domain (base URL) of the Cockpit.
+     |
+     */
+    'domain' => env('COCKPIT_DOMAIN'),
+
+    /*
+     |--------------------------------------------------------------------------
+     | Token
+     |--------------------------------------------------------------------------
+     |
+     | This setting refers to the token related to the project where errors will be created.
+     |
+     */
+    'token' => env('COCKPIT_TOKEN'),
 ];
